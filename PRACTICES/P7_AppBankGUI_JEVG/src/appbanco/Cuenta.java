c8dd786fe84d6f90c76c026dbc0fa85488705f83
JoseEmmanuelVG <89156254+JoseEmmanuelVG@users.noreply.github.com>
2024-07-15 03:42:05 -0600

    Add files via upload

---
+++ PRACTICES/P7_AppBankGUI_JEVG/src/appbanco/Cuenta.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package appbanco;
+
+/**
+ *
+ * @author Israel Cianca
+ */
+public abstract class Cuenta {
+    protected int numcuenta;
+    protected double saldo;
+    public boolean AsignarNumeroCuenta(int d){
+        if (d>0){
+            numcuenta=d;
+            return true;
+        }
+        return false;
+    }
+    public boolean AbonarDinero(double d){
+        if (d>0){
+            saldo=saldo+d;
+            return true;
+        }
+        return false;
+    }
+    public boolean RetirarDinero(double d){
+        if (saldo>=d){
+            saldo=saldo-d;
+            return true;
+        }
+        return false;
+    }
+    public double ObtenerSaldo(){
+        return saldo;
+    }
+    public int ObtenerNumCuenta(){
+        return numcuenta;
+    } 
+}
